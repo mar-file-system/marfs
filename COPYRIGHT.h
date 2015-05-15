9b028658e7f8e900911f9443cbd59bc1c7f46ac2
Brett M. Kettering <brettk@lanl.gov>
2015-05-15 15:37:59 -0600

    After clarification, we can use BSD. Made changes.

--- COPYRIGHT.h
+++ COPYRIGHT.h
@@ -2,26 +2,53 @@
 #define __MARFS_COPYRIGHT_H__
 
 /*
-This file is part of MarFS.
+This file is part of MarFS, which is released under the BSD license.
 
-MarFS is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Lesser Public License
-as published by the Free Software Foundation; either version
-3.0 of the License, or (at your option) any later version.
 
-MarFS is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
+Copyright (c) 2015, Los Alamos National Security (LANS), LLC
+All rights reserved.
 
-You should have received a copy of the GNU Lesser General Public
-License along with MarFS.  If not, see <http://www.gnu.org/licenses/>.
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+-----
+NOTE:
+-----
+MarFS uses libaws4c for Amazon S3 object communication. The original version
+is at https://aws.amazon.com/code/Amazon-S3/2601 and under the LGPL license.
+LANS, LLC added functionality to the original work. The original work plus
+LANS, LLC contributions is found at https://github.com/jti-lanl/aws4c.
+
+GNU licenses can be found at <http://www.gnu.org/licenses/>.
+
 
 From Los Alamos National Security, LLC:
 LA-CC-15-039
 
-Copyright (c) 2009, Los Alamos National Security, LLC All rights reserved.
-Copyright 2009. Los Alamos National Security, LLC. This software was produced
+Copyright (c) 2015, Los Alamos National Security, LLC All rights reserved.
+Copyright 2015. Los Alamos National Security, LLC. This software was produced
 under U.S. Government contract DE-AC52-06NA25396 for Los Alamos National
 Laboratory (LANL), which is operated by Los Alamos National Security, LLC for
 the U.S. Department of Energy. The U.S. Government has rights to use,
