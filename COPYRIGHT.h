0d2bee68b83e2fa14971b708b20d84bfbd312131
Brett M. Kettering <brettk@lanl.gov>
2015-05-15 13:19:07 -0600

    Updated to use the LGPL license.

--- COPYRIGHT.h
+++ COPYRIGHT.h
@@ -2,10 +2,24 @@
 #define __MARFS_COPYRIGHT_H__
 
 /*
+This file is part of MarFS.
+
+MarFS is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Lesser Public License
+as published by the Free Software Foundation; either version
+3.0 of the License, or (at your option) any later version.
+
+MarFS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with MarFS.  If not, see <http://www.gnu.org/licenses/>.
+
+From Los Alamos National Security, LLC:
 LA-CC-15-039
 
-BSD Open Source Licensce applies to this software.
- 
 Copyright (c) 2009, Los Alamos National Security, LLC All rights reserved.
 Copyright 2009. Los Alamos National Security, LLC. This software was produced
 under U.S. Government contract DE-AC52-06NA25396 for Los Alamos National
@@ -17,22 +31,6 @@ ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.  If software is
 modified to produce derivative works, such modified software should be
 clearly marked, so as not to confuse it with the version available from
 LANL.
-
-Additionally, redistribution and use in source and binary forms, with or
-without modification, are permitted provided that the following conditions are
-met:
-
-•   Redistributions of source code must retain the above copyright notice,
-this list of conditions and the following disclaimer.
-
-•   Redistributions in binary form must reproduce the above copyright notice,
-this list of conditions and the following disclaimer in the documentation
-and/or other materials provided with the distribution.
-
-•   Neither the name of Los Alamos National Security, LLC, Los Alamos National
-Laboratory, LANL, the U.S. Government, nor the names of its contributors may be
-used to endorse or promote products derived from this software without specific
-prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY LOS ALAMOS NATIONAL SECURITY, LLC AND CONTRIBUTORS
 "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
