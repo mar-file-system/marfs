d139c38175ec4d26d641ca24bdaaa7a45cce322c
Jeff Inman <jti@lanl.gov>
2018-07-17 11:02:11 -0600

    Restore original copyright boilerplate, in fuse main.c
    Also pulled out unnecessary semis in macros.

--- fuse/src/main.c
+++ fuse/src/main.c
@@ -1,17 +1,24 @@
 /*
 This file is part of MarFS, which is released under the BSD license.
+
+
 Copyright (c) 2015, Los Alamos National Security (LANS), LLC
 All rights reserved.
+
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
+
 1. Redistributions of source code must retain the above copyright notice, this
 list of conditions and the following disclaimer.
+
 2. Redistributions in binary form must reproduce the above copyright notice,
 this list of conditions and the following disclaimer in the documentation and/or
 other materials provided with the distribution.
+
 3. Neither the name of the copyright holder nor the names of its contributors
 may be used to endorse or promote products derived from this software without
 specific prior written permission.
+
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -22,6 +29,7 @@ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
 LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 -----
 NOTE:
 -----
@@ -29,9 +37,13 @@ MarFS uses libaws4c for Amazon S3 object communication. The original version
 is at https://aws.amazon.com/code/Amazon-S3/2601 and under the LGPL license.
 LANS, LLC added functionality to the original work. The original work plus
 LANS, LLC contributions is found at https://github.com/jti-lanl/aws4c.
+
 GNU licenses can be found at <http://www.gnu.org/licenses/>.
+
+
 From Los Alamos National Security, LLC:
 LA-CC-15-039
+
 Copyright (c) 2015, Los Alamos National Security, LLC All rights reserved.
 Copyright 2015. Los Alamos National Security, LLC. This software was produced
 under U.S. Government contract DE-AC52-06NA25396 for Los Alamos National
@@ -43,6 +55,7 @@ ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.  If software is
 modified to produce derivative works, such modified software should be
 clearly marked, so as not to confuse it with the version available from
 LANL.
+
 THIS SOFTWARE IS PROVIDED BY LOS ALAMOS NATIONAL SECURITY, LLC AND CONTRIBUTORS
 "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -106,11 +119,11 @@ OF SUCH DAMAGE.
 
 // This version doesn't call push/pop_groups()
 #define WRAP(FNCALL)                                   \
-   WRAP_internal(0, (FNCALL));
+   WRAP_internal(0, (FNCALL))
 
 // This version DOES call push/pop_groups()
 #define WRAP_PLUS(FNCALL)                              \
-   WRAP_internal(1, (FNCALL));
+   WRAP_internal(1, (FNCALL))
 
 
 
@@ -754,6 +767,7 @@ int fuse_write(const char*            path,
    size_t wk_size = size;
    if (wk_size == (128 * 1024))
       wk_size -= 96;
+
    WRAP( marfs_write(path, buf, wk_size, offset, (MarFS_FileHandle*)ffi->fh) );
 #endif
 }
