2033fa090b00de8d8a65208a86b9cd4d24154a86
Jeff Inman <jti@lanl.gov>
2018-09-07 17:51:37 -0600

    marfs_release*() should detect failures closing objects.
    This corresponds with fixes diagnosed by @thewacokid and @gransom,
    pushed independently to 'master' (f838fdfd6d695).  In the current
    branch, some of the other features they fixed are not needed or are
    obsolete.  Pushing these fixes here, so the eventual merge-conflict
    resolution might be more clear.

--- fuse/src/marfs_ops.c
+++ fuse/src/marfs_ops.c
@@ -2159,7 +2159,7 @@ int marfs_release (const char*        path,
    // been called on this object.
    if( !(fh->flags & FH_FLUSHED) ) {
       LOG(LOG_INFO, "flushing unflushed stream\n");
-      marfs_flush(path, fh);
+      TRY0( marfs_flush(path, fh) );
       EXIT();
       return 0;
    }
@@ -2273,10 +2273,10 @@ int marfs_release_fh(MarFS_FileHandle* fh) {
    ObjectStream*     os   = &fh->os;
 
    if(fh->os.flags & OSF_OPEN) {
-     // Opens are defered.
-     // If open_data wasn't called fh->dal_handle.dal will be NULL
-     // prevent problems by not closing unopened streams
-     close_data(fh, 0, 1);
+      // Opens are defered.
+      // If open_data wasn't called fh->dal_handle.dal will be NULL
+      // prevent problems by not closing unopened streams
+      TRY0( close_data(fh, 0, 1) );
    }
 
    // free aws4c resources
