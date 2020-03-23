2e57eef0030eb3fad86c0bee5b58fa0bc915531b
Shane Goff <drunknirish@live.com>
2020-03-23 09:23:48 -0600

    Get install document online (#216)
    * add install document
    
    Co-authored-by: Brofessional <rgoff@lanl.gov>

--- doc-src/conf.py
+++ doc-src/conf.py
@@ -55,3 +55,6 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
+html_show_sphinx = False
