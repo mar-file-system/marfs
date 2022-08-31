a74908af15b9f50b5777ebd5c17baafccb5060bb
gransom <gransom@lanl.gov>
2022-08-31 07:28:32 -0600

    Minor stream walking changes

--- src/rsrc_mgr/resourceprocessing.c
+++ src/rsrc_mgr/resourceprocessing.c
@@ -82,7 +82,7 @@ typedef struct streamwalker_struct {
    // initialization info
    marfs_ns*  ns;
    MDAL_CTXT  ctxt;
-   time_t     gcthresh;      // time threshold for GCing a file ( none performed, if zero )
+   time_t     gcthresh;      // time threshold for GCing a file ( none performed if zero )
    time_t     repackthresh;  // time threshold for repacking a file ( none performed if zero )
    // quota info
    size_t     inodecnt;
@@ -90,16 +90,12 @@ typedef struct streamwalker_struct {
    // iteration info
    size_t     fileindex;
    size_t     objindex;
-   size_t     objinitial;
    char*      reftgt;
    FTAG       ftag;
+   // cached info
+   size_t     headerlen;
    char*      ftagstr;
    size_t     ftagstralloc;
-   // location estimation info
-   char       verifyall;
-   size_t     headerlen;
-   size_t     minoffset;
-   size_t     maxoffset;
    // GC info
    opinfo*    gcops;
    opinfo*    gcopstail;
@@ -429,29 +425,37 @@ int process_iteratestreamwalker( streamwalker walker, opinfo* gcops, opinfo* rep
       errno = EINVAL;
       return -1;
    }
+   if ( walker->gcthresh != 0  &&  gcops == NULL ) {
+      LOG( LOG_ERR, "Received NULL gcops reference when the walker is set to produce those operations\n" );
+      errno = EINVAL;
+      return -1;
+   }
+   if ( walker->repackthresh != 0  &&  repackops == NULL ) {
+      LOG( LOG_ERR, "Received NULL repackops reference when the walker is set to produce those operations\n" );
+      errno = EINVAL;
+      return -1;
+   }
    // set up some initial values
    marfs_ms* ms = &(walker->ns->prepo->metascheme);
    MDAL mdal = ms->mdal;
    size_t objfiles = walker->ns->prepo->datascheme.objfiles;
    size_t objsize = walker->ns->prepo->datascheme.objsize;
    size_t dataperobj = walker->ftag.objsize - walker->recovheaderlen;
-   size_t lastverified = walker->ftag.fileno;
-   char pullxattrs = 0;
-   if ( walker->verifyall  ||  walker->ftag.streamid == NULL ) { pullxattrs = 1; }
+   char pullxattrs = ( walker->gcthresh == 0  &&  walker->repackthresh == 0 ) ? 0 : 1;
    // iterate over all reference targets
    while ( walker->reftgt != NULL ) {
-      // pull xattr values if we're close to the end of the current object
-      if ( walker->maxoffset + walker->finfoestimate >= dataperobj ) { pullxattrs = 1; }
+      // keep some state for this loop iteration
+      char opspopulated = 0;
       // pull info for the next reference target
-      char haveftag = pullxattrs;
       char filestate = -1;
       struct stat stval;
       GCTAG gctag;
+      char haveftag = pullxattrs;
       if ( process_getfileinfo( walker, pullxattrs, &(filestate), &(stval), &(gctag) ) ) {
          LOG( LOG_ERR, "Failed to get info for reference target: \"%s\"\n", walker->reftgt );
          return -1;
       }
-      pullxattrs = walker->verifyall; // assume we won't be pulling xattrs again, unless we're verifying everything
+      pullxattrs = ( walker->gcthresh == 0  &&  walker->repackthresh == 0 ) ? 0 : 1; // return to default behavior
       if ( filestate == 0 ) {
          // file doesn't exist ( likely that we skipped a GCTAG on the previous file )
          // decrement to the previous index and make sure to check for xattrs
@@ -460,7 +464,7 @@ int process_iteratestreamwalker( streamwalker walker, opinfo* gcops, opinfo* rep
             LOG( LOG_ERR, "Initial reference target does not exist: \"%s\"\n", walker->reftgt );
             return -1;
          }
-         if ( (walker->fileno - 1) == walker->finfo.fileno ) {
+         if ( (walker->fileno - 1) == walker->ftag.fileno ) {
             // looks like we already pulled xattrs from the previous file, and must not have found a GCTAG
             LOG( LOG_ERR, "Datastream break detected at file number %zu: \"%s\"\n", walker->fileno, walker->reftgt );
             return -1;
@@ -481,77 +485,40 @@ int process_iteratestreamwalker( streamwalker walker, opinfo* gcops, opinfo* rep
          pullxattrs = 1;
          continue;
       }
-      // check for possible object transition
-      if ( !(haveftag)  &&  (stval.st_size + walker->maxoffset + walker->finfoestimate >= dataperobj) ) {
-         // object transition is too likely, we need to pull the FTAG of this file
-         LOG( LOG_INFO, "Pulling xattrs from fileno %zu due to likely object transition\n", walker->fileno );
-         pullxattrs = 1;
-         continue;
-      }
-      // check for verified object transition
-      opinfo* dispatchops = NULL;
-      if ( haveftag  &&  walker->ftag.objno != walker->objno ) {
-         // this is only acceptible if we have either verified the previous file OR
-         //    BOTH the previous file referenced the previous object AND this file 
-         //    is at the head of the next object
-         if ( walker->fileno != lastverified  &&
-              ( walker->objno + 1 != walker->ftag.objno  ||  walker->ftag.offset != walker->recovheaderlen ) ) {
-            // this datastream has an unusual structure
-            // safest to return to the most recently verified file and to verify every subsequent file
-            walker->verifyall = 1;
-            if ( walker->activeindex > ( walker->fileno - walker->objinitial ) ) {
-               walker->activeindex -= ( walker->fileno - walker->objinitial ); // decrement offset to match new position
-            }
-            else if ( walker->gcops ) {
-               walker->activeindex = 
-            }
-            walker->fileno = walker->objinitial;  // update position
-            walker->inodecnt -= walker->activefiles;
-            walker->bytecnt -= walker->activebytes;
-            walker->activefiles = 0;
-            walker->activebytes = 0;
-            walker->minoffset = 0;
-            walker->maxoffset = 0;
-            // clear all unissued operations
-            statelog_freeopinfo( walker->gcops );
-            statelog_freeopinfo( walker->rpckops );
-            walker->gcops = NULL;
-            walker->gcopstail = NULL;
-            walker->rpckops = NULL;
-            walker->rpckopstail = NULL;
-            // generate the new reference target path
-            char* origtgt = walker->reftgt;
-            FTAG tmptag = walker.ftag;
-            tmptag.fileno = walker->fileno;
-            walker->reftgt = datastream_genrpath( &(tmptag), ms );
-            if ( walker->reftgt == NULL ) {
-               LOG( LOG_ERR, "Failed to generate ref path tgt for fileno %zu for emergency rescan\n", walker->fileno );
-               return -1;
-            }
-            free( origtgt );
-            continue;
+      // many checks are only appropriate if we're pulling xattrs
+      if ( haveftag ) {
+         // check for innapropriate FTAG value
+         if ( walker->ftag.fileno != walker->fileno ) {
+            LOG( LOG_ERR, "Invalid FTAG filenumber (%zu) on file %zu\n", walker->ftag.fileno, walker->fileno );
+            return -1;
          }
-         // we need to dispatch any previous operations
-         if ( walker->gcops ) {
-            dispatchops = walker->gcops;
-            if ( walker->activefiles == 0 ) {
-               // need to append an object deletion operation
-               walker->gcopstail->next = malloc( sizeof( struct opinfo_struct ) );
-               if ( walker->gcopstail->next == NULL ) {
-                  LOG( LOG_ERR, "Failed to allocate object deletion op for object %zu\n" );
-                  return -1;
+         // check for object transition
+         if ( walker->ftag.objno != walker->objno ) {
+            // we need to dispatch any previous operations
+            if ( walker->gcops ) {
+               if ( walker->activefiles == 0 ) {
+                  // need to prepend one or more object deletion operations
+                  opinfo* objdelop = malloc( sizeof( struct opinfo_struct ) );
+                  if ( objdelop == NULL ) {
+                     LOG( LOG_ERR, "Failed to allocate object deletion op for object %zu\n" );
+                     return -1;
+                  }
+                  objdelop->type = MARFS_DELETE_OBJ_OP;
+                  objdelop->extendedinfo = NULL;
+                  objdelop->start = 1;
+                  objdelop->count = 0;
+                  objdelop->errval = 0;
+                  objdelop->ftag = walker->gcopstail->ftag;
+                  objdelop->ftag.objno = walker->objno;
+                  objdelop->next = walker->gcops;
+                  *gcops = objdelop;
                }
-               opinfo* objdelop = walker->gcopstail->next;
-               objdelop->type = MARFS_DELETE_OBJ_OP;
-               objdelop->extendedinfo = NULL;
-               objdelop->start = 1;
-               objdelop->count = 0;
-               objdelop->errval = 0;
-               objdelop->ftag = walker->gcops->ftag;
-               objdelop->ftag.objno = walker->objno;
-               objdelop->next = NULL;
+               else {
+                  *gcops = walker->gcops;
+               }
+               walker->gcops = NULL; // remove the gc ops from the walker handle
+               opspopulated = 1;
             }
-            walker->gcops = NULL; // remove the gc ops from the walker handle
          }
       }
       else if ( filestate == 1 ) {
