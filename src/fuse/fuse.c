e82df822d2dba253a4086dcfa4b0849308b5b18e
gransom <gransom@lanl.gov>
2022-11-09 07:34:55 -0700

    Very minor fix to get debug build working again

--- src/fuse/fuse.c
+++ src/fuse/fuse.c
@@ -630,7 +630,7 @@ int fuse_read(const char *path, char *buf, size_t size, off_t offset, struct fus
 
   if (rres < 0)
   {
-    LOG(LOG_ERR, "%s\n", strerror(err));
+    LOG(LOG_ERR, "%s\n", strerror(errno));
     ret = (errno) ? -errno : -ENOMSG;
   }
   else { ret = (int)rres; }
